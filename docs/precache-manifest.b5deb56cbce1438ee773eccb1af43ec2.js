2e6e97dda370ef65144c9be29342d3300a251eaa
David Edison Quispe Gutierrez <davidquispeg@gmail.com>
2020-08-24 15:20:58 -0500

    commit for production

---
+++ docs/precache-manifest.b5deb56cbce1438ee773eccb1af43ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dbac780974cca80982003626eabe25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d8ddb486cd5e636d58",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "766a24a36a8ddac31f56",
+    "url": "/static/js/2.ed5bbafc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed5bbafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d8ddb486cd5e636d58",
+    "url": "/static/js/main.ac3aa536.chunk.js"
+  },
+  {
+    "revision": "8fbd23898c71323fe1aa",
+    "url": "/static/js/runtime-main.4c8be365.js"
+  }
+]);
